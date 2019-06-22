9fe8a54a0848acbea93616e1347b505eca04e810
gautam417 <gautammehta@Gautams-MacBook.local>
2019-06-21 18:30:53 -0700

    Implementing React

---
+++ muscleContainer.js
@@ -0,0 +1,202 @@
+const fixedLength = "200px";
+
+const pics = {
+	abductors: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/abductors.jpg",
+	abs: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/abs_0.jpg",
+	adductors: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/adductors.jpg", 
+	biceps: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/biceps_0.jpg", 
+	calves: "https://www.muscleandstrength.com/exercises/calves", 
+	chest: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/chest_0.jpg", 
+	forearms: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/forearms_0.jpg", 
+	glutes: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/glutes_0.jpg", 
+	hamstrings: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/hamstrings_0.jpg", 
+	hipFlexors: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/hipflexors.jpg", 
+	ItBand: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/itband.jpg",
+	lats: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/lats_0.jpg", 
+	lowerBack: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/lowerback.jpg", 
+	upperBack: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/upperback.jpg", 
+	neck: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/neck.jpg",
+	obliques: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/obliques.jpg",
+	palmarFascia: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/palmarfacsia.jpg",
+	plantarFascia: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/plantarfascia.jpg",
+	quads: :"https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/quads_1.jpg",
+	shoulders:"https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/shoulders_0.jpg",
+	traps: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/traps_0.jpg",
+	triceps: "https://cdn.muscleandstrength.com/sites/default/files/taxonomy/image/videos/triceps_0.jpg"
+};
+
+const abductors = (
+	<img 
+	src ={pics.abductors}
+	alt = "Abductors"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const abs = (
+	<img 
+	src ={pics.abs}
+	alt = "Abs"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const adductors = (
+	<img 
+	src ={pics.adductors}
+	alt = "Adductors"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const biceps = (
+	<img 
+	src ={pics.biceps}
+	alt = "Biceps"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const calves = (
+	<img 
+	src ={pics.calves}
+	alt = "Calves"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const chest = (
+	<img 
+	src ={pics.chest}
+	alt = "Chest"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const forearms = (
+	<img 
+	src ={pics.forearms}
+	alt = "Forearms"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const glutes = (
+	<img 
+	src ={pics.glutes}
+	alt = "Glutes"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const hamstrings = (
+	<img 
+	src ={pics.hamstrings}
+	alt = "Hamstrings"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const hipFlexors = (
+	<img 
+	src ={pics.hipFlexors}
+	alt = "Hip Flexors"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const ItBand = (
+	<img 
+	src ={pics.ItBand}
+	alt = "IT Band"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const lats = (
+	<img 
+	src ={pics.lats}
+	alt = "Lats"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const lowerBack = (
+	<img 
+	src ={pics.lowerBack}
+	alt = "Lower Back"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const upperBack = (
+	<img 
+	src ={pics.upperBack}
+	alt = "Upper Back"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const neck = (
+	<img 
+	src ={pics.neck}
+	alt = "Neck"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const obliques = (
+	<img 
+	src ={pics.obliques}
+	alt = "Obliques"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const palmarFascia = (
+	<img 
+	src ={pics.palmarFascia}
+	alt = "Palmar Fascia"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const plantarFascia = (
+	<img 
+	src ={pics.plantarFascia}
+	alt = "Plantar Fascia"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const quads = (
+	<img 
+	src ={pics.quads}
+	alt = "Quads"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const shoulders = (
+	<img 
+	src ={pics.shoulders}
+	alt = "Shoulders"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const traps = (
+	<img 
+	src ={pics.traps}
+	alt = "Traps"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
+
+const triceps = (
+	<img 
+	src ={pics.triceps}
+	alt = "Triceps"
+	height = {fixedLength}
+	width  = {fixedLength}/>
+);
